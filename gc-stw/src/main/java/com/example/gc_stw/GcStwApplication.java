272d8680b0b71b0c64b8d22c0f0e030e9cc8eab4
Waltor <longmengua@users.noreply.github.com>
2025-10-26 04:58:49 +0800

    init

---
+++ gc-stw/src/main/java/com/example/gc_stw/GcStwApplication.java
@@ -0,0 +1,13 @@
+package com.example.gc_stw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcStwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcStwApplication.class, args);
+	}
+
+}
---
+++ gc-stw/src/test/java/com/example/gc_stw/GcStwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gc_stw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcStwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
